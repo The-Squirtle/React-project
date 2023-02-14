f4955416d2e1883aa2242e2eeb7bbd031a374ee7
The-Squirtle <aabde2500@gmail.com>
2023-02-14 14:55:16 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>CV - Amine Nasry</h1>
+      <p>Bienvenue !</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
